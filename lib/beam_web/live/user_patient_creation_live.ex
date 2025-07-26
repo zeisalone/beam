9af857a23cdf9c3dd41e9e07b7fc0b6b50a66621
zeisalone <jmbarbosa2002@gmail.com>
2025-07-26 09:58:41 +0100

    Patient Creation Fix

--- lib/beam_web/live/user_patient_creation_live.ex
+++ lib/beam_web/live/user_patient_creation_live.ex
@@ -16,19 +16,12 @@ defmodule BeamWeb.UserPatientCreationLive do
 
       <.simple_form for={@form} id="patient_creation_form" phx-submit="save" phx-change="validate">
         <.input field={@form[:name]} type="text" label="Nome" required />
+        <input type="hidden" name="user[type]" value="Paciente" />
         <.input field={@form[:email]} type="email" label="Email" required />
         <.input field={@form[:password]} type="password" label="Palavra-passe" required />
-        <.input field={@form[:type]} type="select" label="Tipo" options={["Paciente"]} required />
         <.input field={@form[:birth_date]} type="date" label="Data de Nascimento" required />
         <.input field={@form[:gender]} type="select" label="Género" options={["Masculino", "Feminino", "Outro"]} required />
         <.input field={@form[:education_level]} type="select" label="Escolaridade" options={["Pré-Primaria", "1º ciclo", "2º ciclo", "3º ciclo", "Secundário", "Universitário"]} required />
-        <.input
-          field={@form[:therapist_id]}
-          type="select"
-          label="Terapeuta Atribuído"
-          options={Enum.map(@terapeutas, &{&1.user.name, &1.therapist_id})}
-          required
-        />
         <:actions>
           <.button phx-disable-with="A criar paciente..." class="w-full">Criar Paciente</.button>
         </:actions>
@@ -43,30 +36,54 @@ defmodule BeamWeb.UserPatientCreationLive do
     """
   end
 
-  def mount(_params, _session, socket) do
-    terapeutas = Accounts.list_terapeutas()
-    changeset = Accounts.change_user_registration(%User{})
-    form = to_form(changeset, as: "user")
+  def mount(_params, session, socket) do
+    user_token = session["user_token"]
+    current_user =
+      if user_token do
+        Beam.Accounts.get_user_by_session_token(user_token)
+      else
+        nil
+      end
 
-    {:ok, assign(socket, terapeutas: terapeutas, form: form, full_screen?: false, patient_created: false)}
+    therapist = if current_user, do: Accounts.get_therapist_by_user_id(current_user.id), else: nil
+
+    if is_nil(current_user) or is_nil(therapist) do
+      {:ok, push_navigate(socket, to: "/dashboard")}
+    else
+      changeset = Accounts.change_user_registration(%User{type: "Paciente"})
+      socket =
+        socket
+        |> assign(
+          therapist_id: therapist.therapist_id,
+          form: to_form(changeset, as: "user"),
+          check_errors: false,
+          patient_created: false,
+          full_screen?: false,
+          created_user: nil
+        )
+      {:ok, socket, temporary_assigns: [form: nil]}
+    end
   end
+
 
   def handle_event("validate", %{"user" => user_params}, socket) do
     changeset =
-      %User{}
-      |> Accounts.change_user_registration(user_params)
+      Accounts.change_user_registration(%User{}, Map.put(user_params, "type", "Paciente"))
       |> Map.put(:action, :validate)
 
-    form = to_form(changeset, as: "user")
-    {:noreply, assign(socket, form: form)}
+    {:noreply, assign(socket, form: to_form(changeset, as: "user"), check_errors: !changeset.valid?)}
   end
 
   def handle_event("save", %{"user" => user_params}, socket) do
+    user_params = Map.put(user_params, "type", "Paciente")
+    therapist_id = socket.assigns.therapist_id
+
     case Date.from_iso8601(user_params["birth_date"]) do
       {:ok, birth_date} ->
         updated_params = Map.put(user_params, "birth_date", birth_date)
-        gender = user_params["gender"] || "Masculino"
-        education_level = user_params["education_level"] || "Pré-Primaria"
+        gender = updated_params["gender"] || "Masculino"
+        education_level = updated_params["education_level"] || "Pré-Primaria"
+
         case Accounts.register_user(updated_params) do
           {:ok, user} ->
             {:ok, _} =
@@ -82,7 +99,13 @@ defmodule BeamWeb.UserPatientCreationLive do
               |> assign(trigger_submit: true, patient_created: true, created_user: user)
               |> assign_form(changeset)
 
-            case Accounts.verify_user_type(user.id, user_params["therapist_id"], birth_date, gender, education_level) do
+            case Accounts.verify_user_type(
+                  user.id,
+                  therapist_id,
+                  birth_date,
+                  gender,
+                  education_level
+                ) do
               {:ok, :ok} ->
                 {:noreply, push_navigate(socket, to: ~p"/dashboard")}
 
@@ -94,7 +117,7 @@ defmodule BeamWeb.UserPatientCreationLive do
             end
 
           {:error, %Ecto.Changeset{} = changeset} ->
-            {:noreply, socket |> assign(check_errors: true) |> assign_form(changeset)}
+            {:noreply, assign(socket, check_errors: true, form: to_form(changeset, as: "user"))}
         end
 
       {:error, _reason} ->
@@ -103,12 +126,6 @@ defmodule BeamWeb.UserPatientCreationLive do
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
-    form = to_form(changeset, as: "user")
-
-    if changeset.valid? do
-      assign(socket, form: form, check_errors: false)
-    else
-      assign(socket, form: form)
-    end
+    assign(socket, form: to_form(changeset, as: "user"))
   end
 end
